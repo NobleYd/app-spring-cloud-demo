53598296eb2becffd97be5648c903494559bcbb7
yi zhao <hinobleyd@gmail.com>
2017-07-31 18:55:28 +0800

    ...

--- app-spring-cloud-demo/app-service-b-provider/src/main/java/com/app/ServiceBProviderApplication.java
+++ app-spring-cloud-demo/app-service-b-provider/src/main/java/com/app/ServiceBProviderApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-//注意虽然本服务不需要调用其他服务，但以后可能用，所以引入了feign。
+// 注意虽然本服务不需要调用其他服务，但以后可能用，所以引入了feign。
 public class ServiceBProviderApplication {
 
 	public static void main(String[] args) {
